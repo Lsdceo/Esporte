709e27edb687175ff27ec8592358dd66ec9c5e17
Lucas da Silva <160244553+Lsdceo@users.noreply.github.com>
2024-10-18 10:39:55 -0300

    Add files via upload

---
+++ Esporte/src/main/java/com/Esporte/Esporte/EsporteApplication.java
@@ -0,0 +1,13 @@
+package com.Esporte.Esporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsporteApplication.class, args);
+	}
+
+}
---
+++ Esporte/src/test/java/com/Esporte/Esporte/EsporteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Esporte.Esporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsporteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
